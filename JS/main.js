0b1b45b6092d099f338d729421a7aeabccfffe47
unknown <shy900707@gmail.com>
2026-01-22 21:56:10 +0300

    new save

--- JS/main.js
+++ JS/main.js
@@ -16,3 +16,33 @@ span.appendChild(theleter)
 span.className='Letter-box'
 LetreesContenar.appendChild(span)
 })
+
+// object of words
+
+const words = {
+    porograming:["Javascript","java","python","rust","c++"],
+    sereis:["Game of thrones","the walking dead","preson break","Dexter Morgan"],
+    game:["GTA","Grand sanAndres","Red dead redemtion","hollow knight"],
+    contres:["Saudi arabic","egy","amarica","palsten","srya"],
+    people:["Albert","Nekolas","Nableon","kaysr"]
+}
+
+
+let allkeys = Object.keys(words)
+
+ 
+let randomNumber = Math.floor(Math.random() * allkeys.length)
+
+let randomName = allkeys[randomNumber]
+
+let randomvalue = words[randomName]
+
+let randomvalueNumber = Math.floor(Math.random() * randomvalue.length)
+
+let randomvaluenames = randomvalue[randomvalueNumber]
+
+// gatgory catgory
+
+document.querySelector(".game-info .category span").innerHTML = randomName
+
+
