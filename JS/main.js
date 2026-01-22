5f775caea8cafe369ae3654c98453033598dde85
unknown <shy900707@gmail.com>
2026-01-22 15:59:01 +0300

    sever

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,18 @@
-const leteers = 'abcdefghijklmnopqrsduvwsyz'
+const leteers = 'abcdefghijklmnopqrsduvwxyz';
 
+
+let leteersArray = Array.from(leteers)
+
+
+let LetreesContenar = document.querySelector(".letters")
+
+leteersArray.forEach(leteer => {
+     // create span
+    let span = document.createElement("span")
+
+    // create Letter Text node
+let theleter = document.createTextNode(leteer)
+span.appendChild(theleter)
+span.className='Letter-box'
+LetreesContenar.appendChild(span)
+})
